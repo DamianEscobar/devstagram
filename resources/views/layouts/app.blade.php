fdeae41373f3fe07441c82405093b13a7b3ee6cd
Damian Escobar <damianescobar2001@gmail.com>
2025-02-09 19:39:32 -0300

    Avanzando el formulario de crear cuenta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,13 +16,13 @@
     <body class="bg-gray-300">
         <header class="p-5 border-b bg-white shadow">
             <div class="container mx-auto flex justify-between items-center">
-                <h1 class="text-3xl font-black">
+                <a class="text-3xl font-black" href="/">
                     DevStagram
-                </h1>
+                </a>
 
                 <nav class="flex gap-3 items-center">
                     <a class="font-bold uppercase text-gray-600 text-sm" href="#">Login</a>
-                    <a class="font-bold uppercase text-gray-600 text-sm" href="#">Crear Cuenta</a>
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="/crear-cuenta">Crear Cuenta</a>
                 </nav>
 
             </div>
@@ -34,7 +34,7 @@
             @yield('contenido')
         </main>
 
-        <footer class="text-center p-5 text-gray-500 font-bold uppercase">
+        <footer class="text-center p-5 text-gray-500 font-bold uppercase mt-10">
             DevStagram - Todo los derechos reservados {{ now()->year }}
         </footer>
 
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/', function (){
     return view('principal');
 });
+Route::get('/crear-cuenta', [RegisterController::class, 'index']);
+Route::post('/crear-cuenta', [RegisterController::class, 'store']);
