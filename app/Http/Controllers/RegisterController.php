5c9b130f747f2a900a857d2c452144f722c43522
Damian Escobar <damianescobar2001@gmail.com>
2025-02-28 17:48:53 -0300

    Agregando validacion de campos @error

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,17 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Http\Request;use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 
 class RegisterController extends Controller
 {
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
     public function index() 
     {
         return view('auth.register');
     }
 
-    public function store()
+    public function store(Request $request)
     {
-        dd('Post...');
+        //dd($request);
+        //dd($request->get('username'));
+
+        /** VALIDACION */
+        $request->validate( [
+            'name' => 'required|max:30',
+        ]);
     }
 }
