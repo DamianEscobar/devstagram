660f7dffb12baa6cb5285bcb2c6c3ff8bb52f91d
Damian Escobar <damianescobar2001@gmail.com>
2025-03-05 17:42:03 -0300

    Migrando los datos de la base de datos

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -23,6 +23,8 @@ class RegisterController extends Controller
         /** VALIDACION */
         $request->validate( [
             'name' => 'required|max:30',
+            'username' => 'required|unique:users|min:3|max:20',
+            'email' => 'required|unique:users|email|max:60','password' => 'required'
         ]);
     }
 }
