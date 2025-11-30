17aebf0a5edcb798e95bca9b38fe97f0091cc7ef
Damian Escobar <damianescobar2001@gmail.com>
2025-11-30 16:31:47 -0300

    Se agregan algunos eventos de dropzone, como sending o success. Se crea un archivo "image" lo creo la instalacion de intervetion image. En el regiterController simplemente le paso el nombre del usuario para que no falle el routing ya que pide el nombre del usuario para llevarlo a su muro. Controlador de la imagen, se implementa el intervetion image para cambiar el tamaño de la imagen, se crea una ruta para las imagenes que se subirar y se usa el metodo de intervetion image para subir la imagen al servidor. En los archivos de composer esta la instalacion de intervetion image

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\Laravel\Facades\Image;
 
 class ImagenController extends Controller
 {
@@ -10,6 +12,21 @@ class ImagenController extends Controller
     {
         $imagen = $request->file('file');
 
-        return response()->json(['imagen' => $imagen->extension()]);
+        $nombreImagen = Str::uuid() . "." . $imagen->extension();
+
+        $imagenServidor = Image::read($imagen);
+        $imagenServidor->cover(1000, 1000);
+
+
+        $uploadsPath = public_path('uploads');
+
+        if (!is_dir($uploadsPath)) {
+            mkdir($uploadsPath, 0755, true); // true crea directorios anidados
+        }
+
+        $imagenPath = $uploadsPath . '/' . $nombreImagen;
+        $imagenServidor->save($imagenPath);
+
+        return response()->json(['imagen' => $nombreImagen]);
     }
 }
