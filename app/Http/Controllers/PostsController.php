3cb85ef2096255c758a445ae3daf069683320b60
Damian Escobar <damianescobar2001@gmail.com>
2026-01-04 13:52:04 -0300

    En el controlador de Posts agrego validacion para imagenes y logica para creacion de posts. En el model de post se agrega la relacion con user. En el modelo de User se agrega la relacion con el modelo de post.

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -31,8 +32,26 @@ class PostsController extends Controller
             $request,
             [
                 'titulo' => 'required|max:255',
-                'descripcion' => 'required'
+                'descripcion' => 'required',
+                'imagen' => 'required'
             ]
         );
+
+        /* Post::create([
+            'titulo' => $request->titulo,
+            'descripcion' => $request->descripcion,
+            'imagen' => $request->imagen,
+            'user_id' => auth()->user()->id
+        ]); */
+
+        // Otra forma de crear un registro
+        $post = new Post;
+        $post->titulo = $request->titulo;
+        $post->descripcion = $request->descripcion;
+        $post->imagen = $request->imagen;
+        $post->user_id = auth()->user()->id;
+        $post->save();
+
+        return redirect()->route('posts.index', auth()->user()->username);
     }
 }
