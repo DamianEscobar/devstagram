c46a9fcb8bdf0e75165bf857257de4603253a5df
Damian Escobar <damianescobar2001@gmail.com>
2026-01-04 13:54:12 -0300

    Se agrega la logica para la subida de imagenes al servidor. en la vista se agrega un input hidden para la imagen y el error para la validacion, para que sea visible.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -51,6 +51,15 @@
                     @enderror
                 </div>
 
+                <div class="mb-5">
+                    <input value="{{ old('imagen') }}" name="imagen" type="hidden">
+
+                    @error('imagen')
+                        <p class="bg-red-500 text-white rounded-lg text-sm p-1 text-center mt-1">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
                 <input type="submit" value="Crear Publicación"
                     class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
             </form>
